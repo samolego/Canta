ae3d0e636f81f35310d6302c6ff53183b08819a7
Samo Hribar <34912839+samolego@users.noreply.github.com>
2024-01-24 21:32:15 +0100

    Fixes #26

--- android/app/src/main/kotlin/org/samo_lego/canta/BloatUtils.kt
+++ android/app/src/main/kotlin/org/samo_lego/canta/BloatUtils.kt
@@ -7,9 +7,9 @@ import java.net.URL
 
 class BloatUtils {
     private val BLOAT_URL =
-        "https://raw.githubusercontent.com/0x192/universal-android-debloater/main/resources/assets/uad_lists.json"
+        "https://raw.githubusercontent.com/Universal-Debloater-Alliance/universal-android-debloater-next-generation/main/resources/assets/uad_lists.json"
     private val BLOAT_COMMITS =
-        "https://api.github.com/repos/0x192/universal-android-debloater/commits?path=resources%2Fassets%2Fuad_lists.json"
+        "https://api.github.com/repos/Universal-Debloater-Alliance/universal-android-debloater-next-generation/commits?path=resources%2Fassets%2Fuad_lists.json"
 
     fun fetchBloatList(uadList: File, config: File): JSONArray {
         try {
