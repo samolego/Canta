e1dfd8f815e645c8c96b8cea6106ff583e8cdb85
samolego <34912839+samolego@users.noreply.github.com>
2024-08-27 09:53:57 +0200

    Fixes #74

--- app/src/main/java/org/samo_lego/canta/ui/CantaApp.kt
+++ app/src/main/java/org/samo_lego/canta/ui/CantaApp.kt
@@ -9,8 +9,10 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
--- app/src/main/java/org/samo_lego/canta/ui/component/AppList.kt
+++ app/src/main/java/org/samo_lego/canta/ui/component/AppList.kt
@@ -8,8 +8,10 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
@@ -99,6 +101,9 @@ fun AppList(
                                 showAppDialog = appInfo
                             }
                         )
+                    }
+                    item { 
+                        Spacer(modifier = Modifier.height(48.dp))
                     }
                 }
             } else {
